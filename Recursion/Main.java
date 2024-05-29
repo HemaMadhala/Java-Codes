a8921881a4f8425a7e100037ebc80a30f9542646
Hema chowdhary <hemamadhala7@gmail.com>
2024-05-29 17:28:37 +0530

    java

--- Recursion/Main.java
+++ Recursion/Main.java
@@ -1,17 +1,22 @@
 package Recursion;
 
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
-        int arr[]={1,3};
+        int arr[]={1,2};
         //NaturalNumbers.printNumAsc(0, 10);
         //NaturalNumbers.printNumDes(0, 10);
         //Calc.sumOfNaturalNumbers(0, 3, 0);
         //System.out.println(Factorial.fact(5));
-        //SubSets.sub("aab", 0, "");
+        //SubSets.sub("abc", 0, "");
         //fibonacci.fib(0, 1, 5);
         //System.out.println(XpowN.xpow(2, 5));
         //System.out.println(XpowN.xpownOpt(2, 5));
-        System.out.println(IsSorted.isSorted(arr));
+        //System.out.println(IsSorted.isSorted(arr));
+        ArrayList<ArrayList<Integer>> a=new ArrayList<>();
+        ArrayList<Integer> b=new ArrayList<>();
+        SubSets.subsetofIntegers(arr,0, a, b);
     }
     
 }
--- Recursion/SubSets.java
+++ Recursion/SubSets.java
@@ -1,5 +1,10 @@
 package Recursion;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+
 public class SubSets {
     public static void sub(String s, int idx, String newS){
         if(idx==s.length()){
@@ -12,4 +17,24 @@ public class SubSets {
         sub(s, idx+1, newS+cur);
         sub(s, idx+1, newS);
     }
+
+    public static ArrayList<String> subArray(String s, int idx, ArrayList<String> newArray, String news){
+        return newArray;
+    }
+
+    public static void subsetofIntegers(int[] nums, int i,ArrayList<ArrayList<Integer>> a,ArrayList<Integer> b){
+
+        if(nums.length==i){
+            a.add(new ArrayList<>(b));
+            System.out.println(a);
+            return;
+        }
+        
+        
+        subsetofIntegers(nums, i+1, a, b);
+        b.add(nums[i]);
+        subsetofIntegers(nums, i+1, a, b);
+        b.remove(b.size()-1);
+    }
+
 }
